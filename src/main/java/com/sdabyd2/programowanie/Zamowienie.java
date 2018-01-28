051225651ae2969397aecaa58af110ddca3f30ab
Maciej Swiderski <maciej.swiderski@wp.pl>
2018-01-28 13:03:55 +0100

    Zamowienie

--- src/main/java/com/sdabyd2/programowanie/Zamowienie.java
+++ src/main/java/com/sdabyd2/programowanie/Zamowienie.java
@@ -1,43 +1,73 @@
 package com.sdabyd2.programowanie;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Zamowienie {
 
     int ileDodanych;
     int maksRozmiar;
-    Pozycja[]pozycje=new Pozycja[3];
+    List<Pozycja> pozycja = new ArrayList<>();
+
+//    public List<Pozycja> getPozycja() {
+//        return pozycja;
+//    }
+//
+//    public void setPozycja(List<Pozycja> pozycja) {
+//        this.pozycja = pozycja;
+//    }
 
     public Zamowienie() {
-        this.maksRozmiar=10;
+        this.maksRozmiar = 10;
 
     }
 
     public Zamowienie(int ileDodanych) {
-        this.ileDodanych=ileDodanych;
+        this.ileDodanych = ileDodanych;
     }
 
     public void dodajPozycje(Pozycja p) {
 
-        for (int i = 0; i < ileDodanych; i++) {
-            if (pozycje[i] == null) {
-                this.pozycje[i] = p;
-            }
+        if(pozycja.equals(p)){
+
+            p.ileSztuk++;
+
+        }else{
+            pozycja.add(p);
         }
+
+
     }
 
-    public double obliczWartoscCala(){
 
-        double calkowitaWartoscZamowienia=0;
+    public double obliczWartoscCala() {
 
-        for(int i=0;i<pozycje.length;i++){
-            calkowitaWartoscZamowienia=this.pozycje[i].obliczWartosc();
+
+        double calkowitaWartoscZamowienia = 0;
+
+        for (int i = 0; i < pozycja.size(); i++) {
+            calkowitaWartoscZamowienia += pozycja.get(i).obliczWartosc();
         }
         return calkowitaWartoscZamowienia;
     }
 
     @Override
     public String toString() {
-        return  "Zamowienie :\n"+Arrays.asList(pozycje)+"\n"+" Razem: " + obliczWartoscCala();
+        return "Zamowienie :\n" + pozycja + "\n" + " Razem: " + obliczWartoscCala() + " zł";
     }
+
+
+    public void usunPozycje(int indeks) {
+        pozycja.remove(pozycja.get(indeks));
+    }
+
+    void edytujPozycje(int indeks,double cena,int sztuk,String nazwa ) {
+
+        pozycja.get(indeks).cena=cena;
+        pozycja.get(indeks).ileSztuk=sztuk;
+        pozycja.get(indeks).nazwaTowaru=nazwa;
+    }
+
+
 }
